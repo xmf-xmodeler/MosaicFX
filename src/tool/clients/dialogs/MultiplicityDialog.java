b696a9cd2699bb1a416107e60b3e3b73107a2be8
dtoepel <toepel@gmx.it>
2018-02-23 16:28:16 +0000

    Repair Multiplicity Dialog

--- src/tool/clients/dialogs/MultiplicityDialog.java
+++ src/tool/clients/dialogs/MultiplicityDialog.java
@@ -89,31 +89,27 @@ public class MultiplicityDialog extends Dialog<ButtonType>{
 //		return "not yet implemented";
 	}
 		
-private String getResult(boolean ok) {
-//		if(ok) {			
-			try{
-				int a = ok?Integer.parseInt(txtMin.getText()):min;
-				if(ok) {
-				if(!"*".equals(txtMax.getText())) {
-					int b = Integer.parseInt(txtMax.getText());
-					if(b < 1) throw new IllegalArgumentException();
-					if(b < a) throw new IllegalArgumentException();
-				} else {}}
-				if(ok){
-					s = txtMin.getText() + ".." + txtMax.getText() + (boxOrdered.isSelected()?"$":"");
-				} else {
-					s = min + ".." + (max<0?"*":(max+"")) + (ordered?"$":"");
-				}
-				if(s.endsWith("..1$")) s = s.substring(0, s.length()-1);
-				if("1..1".equals(s)) s = "!";
-				if("0..1".equals(s)) s = "1";
-				return s;
-			} catch (Exception e) {
-//				s = null;
-				return null;
+	private String getResult(boolean ok) {
+		try{
+			int a = ok?Integer.parseInt(txtMin.getText()):min;
+			if(ok) {
+			if(!"*".equals(txtMax.getText())) {
+				int b = Integer.parseInt(txtMax.getText());
+				if(b < 1) throw new IllegalArgumentException();
+				if(b < a) throw new IllegalArgumentException();
+			} else {}}
+			if(ok){
+				s = (boxOrdered.isSelected()?"$":"") + txtMin.getText() + ".." + txtMax.getText();
+			} else {
+				s = (ordered?"$":"") + min + ".." + (max<0?"*":(max+""));
 			}
-//		}
-		
+			if(s.endsWith("..1") && s.startsWith("$")) s = s.substring(1);
+			if("1..1".equals(s)) s = "!";
+			if("0..1".equals(s)) s = "1";
+			return s;
+		} catch (Exception e) {
+			return null;
+		}
 	}
 
 //	private String showLocal() {
@@ -222,43 +218,22 @@ private String getResult(boolean ok) {
 	}
     
     protected void createDialogArea(GridPane grid) {
-//	    Composite area = (Composite) super.createDialogArea(parent);
-//	    Composite container = new Composite(area, SWT.NONE);
-//	    container.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
-//	    GridLayout layout = new GridLayout(4, false);
-//	    container.setLayout(layout);
     	
     	final ToggleGroup group = new ToggleGroup();
 
     	RadioButton defaultR1 = new RadioButton("0..1"); 
-//	    defaultR1.setText("0..1"); 
-//	    GridData gd = new GridData();
-//	    gd.horizontalSpan=2;
-//	    defaultR1.setLayoutData(gd);
 	    GridPane.setColumnSpan(defaultR1, 2);
 	    grid.add(defaultR1, 0, 0);
 	    defaultR1.setToggleGroup(group);
 	    
         defaultR1.selectedProperty().addListener((event, oldValue, newValue) -> {
         	if(newValue && !oldValue) { setValues("0","1",false); } });
-        //	    defaultR1.addSelectionListener(new SelectionListener() {
-//			@Override public void widgetSelected(SelectionEvent arg0) {setValues("0","1",false);}
-//			@Override public void widgetDefaultSelected(SelectionEvent arg0) {setValues("0","1",false);}
-//		});
 	    
 	    Label label1 = new Label("Zero or one"); 
-//	    label1.setText("Zero or one");
-//	    gd = new GridData();
-//	    gd.horizontalSpan=2;
-//	    label1.setLayoutData(gd);
 	    GridPane.setColumnSpan(label1, 2);
 	    grid.add(label1, 2, 0);
 	    
 	    RadioButton defaultR2 = new RadioButton("1..1"); 
-//	    defaultR2.setText("1..1"); 
-//	    gd = new GridData();
-//	    gd.horizontalSpan=2;
-//	    defaultR2.setLayoutData(gd);
 	    GridPane.setColumnSpan(defaultR2, 2);
 	    grid.add(defaultR2, 0, 1);
 	    defaultR2.setToggleGroup(group);
@@ -271,18 +246,10 @@ private String getResult(boolean ok) {
 //		});
 	    
 	    Label label2 = new Label("Exactly one"); 
-//	    label2.setText("Exactly one"); 
-//	    gd = new GridData();
-//	    gd.horizontalSpan=2;
-//	    label2.setLayoutData(gd);
 	    GridPane.setColumnSpan(label2, 2);
 	    grid.add(label2, 2, 1);
 	    
 	    RadioButton defaultR3 = new RadioButton("0..*"); 
-//	    defaultR3.setText("0..*"); 
-//	    gd = new GridData();
-//	    gd.horizontalSpan=2;
-//	    defaultR3.setLayoutData(gd);
 	    GridPane.setColumnSpan(defaultR3, 2);
 	    grid.add(defaultR3, 0, 2);
 	    defaultR3.setToggleGroup(group);
@@ -295,18 +262,10 @@ private String getResult(boolean ok) {
 //		});
 	    
 	    Label label3 = new Label("Any number including zero"); 
-//	    label3.setText("Any number including zero"); 
-//	    gd = new GridData();
-//	    gd.horizontalSpan=2;
-//	    label3.setLayoutData(gd);
 	    GridPane.setColumnSpan(label3, 2);
 	    grid.add(label3, 2, 2);
 	    
 	    RadioButton defaultR4 = new RadioButton("1..*"); 
-//	    defaultR4.setText("1..*"); 
-//	    gd = new GridData();
-//	    gd.horizontalSpan=2;
-//	    defaultR4.setLayoutData(gd);
 	    GridPane.setColumnSpan(defaultR4, 2);
 	    grid.add(defaultR4, 0, 3);
 	    defaultR4.setToggleGroup(group);
@@ -354,40 +313,23 @@ private String getResult(boolean ok) {
 	    boxOrdered.setSelected(ordered);
 	    boxNavigable = new CheckBox("Navigable"); 
 	    boxNavigable.setSelected(navigable);
-	    
-//	    GridData textFieldGridData = new GridData();
-//	    textFieldGridData.horizontalSpan = 4;
-//	    boxOrdered.setLayoutData(textFieldGridData); 
 
 	    GridPane.setColumnSpan(boxOrdered, 4);
         grid.add(boxOrdered, 0, 5);
 	    
-//	    textFieldGridData = new GridData();
-//	    textFieldGridData.horizontalSpan = 4;
-//	    boxNavigable.setLayoutData(textFieldGridData);
-
 	    GridPane.setColumnSpan(boxNavigable, 4);
         grid.add(boxNavigable, 0, 6);
             
 
-//      return container;
     }
 
-    // overriding this methods allows you to set the
-    // title of the custom dialog
-//    @Override
-//    protected void configureShell(Shell newShell) {
-//      super.configureShell(newShell);
-//      newShell.setText("Selection dialog");
-//    }
-//    
     private void setValues(String min, String max, boolean ordered) {
     	txtMin.setText(min);
     	txtMax.setText(max);
     	boxOrdered.setSelected(ordered);
     	txtMin.setEditable(false);
     	txtMax.setEditable(false);
-    	boxOrdered.setIndeterminate(true);
+//    	boxOrdered.setIndeterminate(true);
     } 
     
     private void enableUserDef() {
@@ -395,19 +337,5 @@ private String getResult(boolean ok) {
     	txtMax.setEditable(true);
     	boxOrdered.setIndeterminate(false);
     }
-//    
-//	@Override
-//	protected void okPressed() {
-//		okPressed = true;
-//		blocked = false;
-////		super.okPressed();
-//	}
-//
-//	@Override
-//	protected void cancelPressed() {
-//		okPressed = false;
-//		blocked = false;
-////		super.cancelPressed();
-//	}
 
 }
