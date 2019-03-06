9870f1a4519ac8e2b2ba963fa31d54be2cebf456
wahidnurhani <wahidnurhani@yahoo.de>
2019-03-06 15:55:53 +0100

    Add Attribute-Dialog init

--- src/tool/clients/fmmlxdiagrams/Palette.java
+++ src/tool/clients/fmmlxdiagrams/Palette.java
@@ -11,6 +11,7 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Priority;
+import tool.clients.fmmlxdiagrams.dialogs.AddAttributeDialog;
 import tool.clients.fmmlxdiagrams.dialogs.AddInstanceDialog;
 import tool.clients.fmmlxdiagrams.dialogs.CreateMetaClassDialog;
 import tool.clients.fmmlxdiagrams.dialogs.MetaClassDialogResult;
@@ -30,7 +31,7 @@ public class Palette extends GridPane {
 		addButton("Add MetaClass", 0, e -> addMetaClassDialog());
 		addButton("Add Instance", 1, e -> addInstanceDialog());
 		addButton("Remove MetaClass/Instance", 2, e -> System.out.println("Button 2"));
-		addButton("Add Attribute", 3, e -> System.out.println("Button 3"));
+		addButton("Add Attribute", 3, e -> addAttributeDialog());
 		addButton("Edit Attribute", 4, e -> System.out.println("Button 4"));
 		addButton("Remove Attribute", 5, e -> System.out.println("Button 5"));
 		addButton("Change Slot Value", 6, e -> System.out.println("Button 6"));
@@ -40,6 +41,24 @@ public class Palette extends GridPane {
 		cc.setHgrow(Priority.ALWAYS);
 		getColumnConstraints().add(cc);
 
+	}
+
+	private void addAttributeDialog() {
+		// TODO Auto-generated method stub
+		CountDownLatch l = new CountDownLatch(1);
+		
+		Platform.runLater(() -> {
+			AddAttributeDialog dlg = new AddAttributeDialog();
+			Optional<MetaClassDialogResult> opt = dlg.showAndWait();
+			
+			if(opt.isPresent()) {
+				MetaClassDialogResult test = opt.get();
+				System.out.println("!!!!!!!!!!!!! " + test.getName() + " " + test.getLevel());
+			}
+			
+			diagram.updateDiagram();
+			l.countDown();
+		});
 	}
 
 	private void addButton(String string, int y, EventHandler<ActionEvent> eventHandler) {
---
+++ src/tool/clients/fmmlxdiagrams/dialogs/AddAttributeDialog.java
@@ -0,0 +1,96 @@
+package tool.clients.fmmlxdiagrams.dialogs;
+
+import javafx.scene.control.TextField;
+import javafx.scene.control.ButtonBar.ButtonData;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DialogPane;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+
+public class AddAttributeDialog extends CustomDialog<MetaClassDialogResult> {
+	
+	private GridPane gridPane = new GridPane();
+	
+	private Label nameLabel ;
+	private Label classLabel;
+	private Label levelLabel;
+	private Label typeLabel;
+	private Label multiplicityLabel;
+	
+	private TextField nameTextField;
+	private ComboBox<String> classCombobox; //TODO Type has to be changed
+	private ComboBox<String> levelComboBox; //TODO type has to be changed
+	private ComboBox<String> typeComboBox; //TODO type has to be changed
+	private ComboBox<String> multiplicityComboBox; //TODO type has to be changed
+	
+	
+	public AddAttributeDialog() {
+		super();
+		
+		DialogPane dialogPane = getDialogPane();
+		dialogPane.setHeaderText("Add Attribute");
+		
+		dialogPane.getButtonTypes().addAll(ButtonType.OK, ButtonType.CANCEL);
+		
+		gridPane = initializeGrid();
+		addElementToGrid();
+
+		dialogPane.setContent(grid);
+		
+		final Button okButton = (Button) getDialogPane().lookupButton(ButtonType.OK);
+		okButton.addEventFilter(ActionEvent.ACTION, e -> {
+			if (!validateUserInput()) {
+				e.consume();
+			}
+		});
+
+		setResultConverter(dlgBtn -> {
+			if (dlgBtn != null && dlgBtn.getButtonData() == ButtonData.OK_DONE) {
+				//TODO
+			}
+			return null;
+		});
+		
+		
+	}
+	private boolean validateUserInput() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	
+	private void addElementToGrid() {
+		nameLabel = new Label("Name");
+		classLabel = new Label("Class");
+		levelLabel = new Label("Level");
+		typeLabel = new Label("Type");
+		multiplicityLabel = new Label("Multiplicity");
+		
+		nameTextField = new TextField();
+		classCombobox = new ComboBox<>();
+		levelComboBox = new ComboBox<>();
+		typeComboBox = new ComboBox<>();
+		multiplicityComboBox = new ComboBox<>();
+		
+		classCombobox.setPrefWidth(COLUMN_WIDTH);
+		levelComboBox.setPrefWidth(COLUMN_WIDTH);
+		typeComboBox.setPrefWidth(COLUMN_WIDTH);
+		multiplicityComboBox.setPrefWidth(COLUMN_WIDTH);
+		
+		gridPane.add(nameLabel, 0, 0);
+		gridPane.add(nameTextField, 1, 0);
+		gridPane.add(classLabel, 0, 1);
+		gridPane.add(classCombobox, 1, 1);
+		gridPane.add(levelLabel, 0, 2);
+		gridPane.add(levelComboBox, 1, 2);
+		gridPane.add(typeLabel, 0, 3);
+		gridPane.add(typeComboBox, 1, 3);
+		gridPane.add(multiplicityLabel, 0, 4);
+		gridPane.add(multiplicityComboBox, 1, 4);
+		
+	}
+
+}
