0a38e65f7d85535d00237b6e46eab1f773253b5d
MaxGertz <max.gertz@gmx.de>
2019-07-09 15:55:19 +0200

    Redesigned AddDialog for operation
    - Added return type comboBox
    - moved StringValueDialog to dialog-package
    - changed type of body to textArea
    - added: button to check if operation body is parsable

--- src/tool/clients/fmmlxdiagrams/dialogs/stringvalue/StringValueDialog.java
+++ src/tool/clients/fmmlxdiagrams/dialogs/stringvalue/StringValueDialog.java
@@ -1,8 +1,8 @@
-package tool.clients.fmmlxdiagrams.stringvalue;
+package tool.clients.fmmlxdiagrams.dialogs.stringvalue;
 
 public class StringValueDialog {
-	
-	public static class ErrorMessage{
+
+	public static class ErrorMessage {
 		//Select
 		public static final String selectNewOwner = "Select New Owner";
 		public static final String selectAttribute = "Select Attribute!";
@@ -14,34 +14,37 @@ public class StringValueDialog {
 		public static final String selectNewType = "Select New Type!";
 		public static final String selectAnotherType = "Select Another Type!";
 		public static final String selectNewParent = "Select New Parent!";
-		
+
 		public static final String enterValidName = "Type Valid Name!";
 		public static final String nameAlreadyUsed = "Name Already Used.";
 
 		public static final String pleaseSelectAnotherLevel = "Please select another level!";
 	}
-	
-	public static class LabelAndHeaderTitle{
+
+	public static class LabelAndHeaderTitle {
 		public static final String selectedObject = "Selected Object";
+		public static final String aClass = "Class";
 		public static final String name = "Name";
-		public static final String level ="Level";
+		public static final String level = "Level";
 		public static final String owner = "Owner";
 		public static final String of = "Of";
 		public static final String parent = "Parent";
 		public static final String type = "Type";
 		public static final String Multiplicity = "Multiplicity";
 		public static final String body = "Body";
-				
+		public static final String operationName = "Operation Name";
+
 		//Current
 		public static final String currentOwner = "Current Owner";
 		public static final String currentLevel = "Current Level";
 		public static final String currentOf = "Current Of";
 		public static final String currentParent = "Current Parent";
-		
+
 		//New
 		public static final String newOwner = "New Owner";
 		public static final String newLevel = "New Level";
-		
+		public static final String newOperation = "New Operation";
+
 		//Change
 		public static final String changeClassLevel = "Change Class Level";
 		public static final String changeOf = "Change Of";
@@ -53,8 +56,8 @@ public class StringValueDialog {
 		public static final String changeOperationType = "Change Operation Type";
 		public static final String changeAssociationLevel = "Change Association Level";
 		public static final String changeAssociationType = "Change Association Type";
-		public static final String changeParent= "Change Parent";
-			
+		public static final String changeParent = "Change Parent";
+
 		//Select
 		public static final String selectAttribute = "Select Attribute";
 		public static final String selectOperation = "Select Operation";
@@ -64,6 +67,17 @@ public class StringValueDialog {
 		public static final String selectType = "Select Type";
 		public static final String selectLevel = "Select Level";
 		public static final String selectNewParent = "Select New Parent";
+
+		public static final String checkSyntax = "Check Syntax";
+	}
+
+	public static class OperationStringValues {
+		public static final String emptyOperation =
+				"@Operation op0():XCore::Element\n" +
+						"  null\n" +
+						"end";
+
+
 	}
 
 }
