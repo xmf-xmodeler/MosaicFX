c823a111476c524caa4cfb6b47dc3319267454b6
wahidnurhani <wahidnurhani@yahoo.de>
2019-11-10 18:22:12 +0100

    update add enum

---
+++ src/tool/clients/fmmlxdiagrams/EnumElement.java
@@ -0,0 +1,20 @@
+package tool.clients.fmmlxdiagrams;
+
+public class EnumElement {
+	
+	private String name;
+
+	public EnumElement(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
--- src/tool/clients/fmmlxdiagrams/dialogs/AddEnumerationDialog.java
+++ src/tool/clients/fmmlxdiagrams/dialogs/AddEnumerationDialog.java
@@ -1,22 +1,33 @@
 package tool.clients.fmmlxdiagrams.dialogs;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.DialogPane;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.util.converter.IntegerStringConverter;
+import tool.clients.fmmlxdiagrams.EnumElement;
 import tool.clients.fmmlxdiagrams.FmmlxDiagram;
 import tool.clients.fmmlxdiagrams.dialogs.results.AddEnumerationDialogResult;
+import javafx.scene.Node;
 
 public class AddEnumerationDialog extends CustomDialog<AddEnumerationDialogResult>{
 	
 	private FmmlxDiagram diagram;
 	private Label nameLabel;
-	private Label elementLabel;
+	private Label numberOfElementLabel;
+	private Label inputElementLabel;
 	
 	private TextField nameTextField;
+	private ComboBox<Integer> numberOfElements;
+	private ListView<EnumElement> inputElementListview;
+	
 
 	public AddEnumerationDialog(FmmlxDiagram diagram) {
 		super();
@@ -25,6 +36,7 @@ public class AddEnumerationDialog extends CustomDialog<AddEnumerationDialogResul
 
 		DialogPane dialogPane = getDialogPane();
 		dialogPane.getButtonTypes().addAll(ButtonType.OK, ButtonType.CANCEL);
+		dialogPane.setHeaderText("Create Enumeration");
 
 		addElementToGrid();
 
@@ -53,6 +65,28 @@ public class AddEnumerationDialog extends CustomDialog<AddEnumerationDialogResul
 	}
 
 	private void addElementToGrid() {
+		nameLabel = new Label("Name");
+		numberOfElementLabel = new Label("Number of Elements");
+		inputElementLabel = new Label("Input Element");
+		
+		nameTextField = new TextField();
+		nameTextField.isEditable();
+		numberOfElements = new ComboBox<Integer>(LevelList.levelList);
+		numberOfElements.setConverter(new IntegerStringConverter());
+		numberOfElements.setEditable(true);
+		
+		List<Node> labelNode = new ArrayList<Node>();
+		List<Node> editorNode = new ArrayList<Node>();
+
+		labelNode.add(nameLabel);
+		labelNode.add(numberOfElementLabel);
+		
+		editorNode.add(nameTextField);
+		editorNode.add(numberOfElements);
+		
+		addNodesToGrid(labelNode,0);
+		addNodesToGrid(editorNode, 1);
+		
 		// TODO Auto-generated method stub
 		
 	}
