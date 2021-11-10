bbd62df6362b9326da89892e64bb8d205b3b5ca1
Daniel Töpel <toepel@gmx.it>
2021-11-10 19:44:19 +0100

    fix

--- src/tool/clients/fmmlxdiagrams/graphics/AbstractSyntax.java
+++ src/tool/clients/fmmlxdiagrams/graphics/AbstractSyntax.java
@@ -97,7 +97,6 @@ public class AbstractSyntax extends NodeGroup{
 			if ("#text".equals(n.getNodeName()) ||  "defs".equals(n.getNodeName()) || "title".equals(n.getNodeName())) {
 				// ignore !!!
 			} else if("SVG".equals(n.getNodeName())) {
-				System.err.println("X");
 				vec.add(readSVG(dir, (Element) n));
 			} else if("Label".equals(n.getNodeName())){
 				vec.add(readLabel((Element) n));
@@ -110,8 +109,7 @@ public class AbstractSyntax extends NodeGroup{
 		
 		ng.addAllNodeElements(vec);
 		if(parent.hasAttribute("id")) ng.id = parent.getAttribute("id");
-		return ng;
-		
+		return ng;		
 	}
 
 	private static SVGGroup readSVG(File dir, Element e)
