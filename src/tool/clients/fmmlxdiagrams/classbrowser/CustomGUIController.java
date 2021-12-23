4f58e7ad32a6b6637d52df33c6aa1b05a52cc643
DrMatti <75906518+DrMatti@users.noreply.github.com>
2021-12-23 11:28:16 +0100

    Introducing custom GUI support for the object browser.
    Currently the object browser is only generating a default GUI. This
    default gui can now be extracted into a FXML document, which is
    accounting for a mapping between identifiers and slots of the
    Fmmlxdiagram.
    
    Furthermore, this extracted FXML document may be adapted in all
    foreseeable manners (except changing controller, ids or actions in an
    unintended way). This might be achieved by reading it within the tool
    "Glu on".
    
    Afterwards, the created custom GUI can be imported into the object
    browser to replace the by default generated GUI. The values and actions
    are still correctly handled by the implemented controller class.

---
+++ src/tool/clients/fmmlxdiagrams/classbrowser/CustomGUIController.java
@@ -0,0 +1,126 @@
+package tool.clients.fmmlxdiagrams.classbrowser;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+import tool.clients.fmmlxdiagrams.AbstractPackageViewer;
+import tool.clients.fmmlxdiagrams.FmmlxObject;
+import tool.clients.fmmlxdiagrams.FmmlxSlot;
+
+public class CustomGUIController {
+	   // These attributes are required to change the diagram/model
+	   AbstractPackageViewer diagram;
+	   FmmlxObject metaClass;
+	   FmmlxObject object;
+	   
+	   // Access to the surrounding object browser and 
+	   // the elements of the custom gui
+	   ObjectBrowser parent;
+	   FXMLLoader loader;
+	   Parent customGUI;
+	   private Map<String, Node> objToID = new HashMap<>(); // required to reference all children with id
+	   
+	   public CustomGUIController(AbstractPackageViewer diagram, FmmlxObject metaClass, FXMLLoader loader, FmmlxObject object , ObjectBrowser parent) {
+		   this.diagram = diagram;
+		   this.metaClass = metaClass;
+		   this.loader = loader;
+		   this.object = object;
+		   this.parent = parent;
+	   }
+	   
+	   @FXML
+	   public void initialize() {
+		   // initialize the missing attributes after the loading of the controller is finished
+		   // determine the id-node-map for access to I/O
+		   customGUI = (Parent) loader.getRoot();
+		   ObservableList<Node> children = customGUI.getChildrenUnmodifiable();
+		   String id;
+		   Node obj;
+		   for(Node currEl: children) {
+			   id = currEl.getId();
+			   if( id != null ) {
+				   // this has to be done this way as the Parent class does not allow read access to its children
+				   obj = customGUI.lookup("#" + id);
+				   objToID.put(id, obj);
+			   }
+		   }
+		   
+		   // now inject the input values for the custom gui based on the slots of the current instance
+		   setObject(this.object);
+		   
+		   // Output information
+		   System.err.println("Load finished!");
+	   }
+	   
+	   public void setObject(FmmlxObject object) {
+		   this.object = object;
+		   
+		   // also set the slots in the text of the TextFields
+		   int i = 1;
+		   String baseID = "tc1r"; 
+		   for(FmmlxSlot slot: object.getAllSlots()) {
+			   try {	  
+				 // read the TextField in the current row from the map attribute
+				 TextField currTextField = (TextField) objToID.get(baseID + i);
+				 currTextField.setText(slot.getValue());
+				 
+			   } catch( Exception e ) {
+				   e.printStackTrace();
+			   }
+			   i++;
+		   }
+	   }
+	   	  
+	  // this is an action, which is called by the buttons in the gui
+	  @FXML private void setSlot(ActionEvent event) { 
+		  event.consume();
+		  
+		  // Get ID of triggering object
+		  String id = ((Node) event.getSource()).getId()  ;
+		  
+		  // Determine column and row from the id
+		  Integer col = Integer.parseInt( id.substring( (id.indexOf('c'))+1, id.indexOf('r')) );
+		  Integer row = Integer.parseInt( id.substring( id.indexOf('r')+1 ));
+		  
+		  // subtract 1 from col as 0 = label, 1 = text, 2 = button
+		  col -= 1;
+		  
+		  // subtract 1 from row as the java index starts at zero
+		  row -= 1;
+		  
+		  // build new id assuming the text field independent from the source
+		  id = "tc" + col + id.substring(id.indexOf('r'));
+		  
+		  // get value from the textfield by using the map
+		  String value = ((TextField) objToID.get(id)).getText();
+		  			
+		  // Now we need the slot, which is corresponding to the required TextField (so the row no)
+		  FmmlxSlot slotToID = object.getAllSlots().get(row);
+		  
+		  // Note: The current implementation of the GUI is only supporting expressions as input.
+		  
+		  // send value to XMF, update the diagram and then update the object browser
+		  diagram.getComm().changeSlotValue(diagram.getID(), object.getName(), slotToID.getName(), value);;
+		  diagram.updateDiagram();
+			
+		  // wait a few moments until update is commited
+		  try {
+			  Thread.sleep(100);
+		  } catch( Exception e ) {
+			  e.printStackTrace();
+		  }
+			
+	      // also update the object list of the parent as it gets outdated otherwise
+		  parent.updateObjectList();
+		}
+}
