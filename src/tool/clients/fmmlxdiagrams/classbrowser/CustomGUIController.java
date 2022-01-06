52dd8d8f29889757cebfaf40d780cf6b9097e66d
DrMatti <75906518+DrMatti@users.noreply.github.com>
2022-01-06 19:36:16 +0100

    CustomGUI Support with recursive parent handling

--- src/tool/clients/fmmlxdiagrams/classbrowser/CustomGUIController.java
+++ src/tool/clients/fmmlxdiagrams/classbrowser/CustomGUIController.java
@@ -12,6 +12,8 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TitledPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 
@@ -45,25 +47,9 @@ public class CustomGUIController {
 		   // initialize the missing attributes after the loading of the controller is finished
 		   // determine the id-node-map for access to I/O
 		   this.customGUI = (Parent) loader.getRoot();
-		   
-		   // required to get children
-		   Parent content;
-		   
-		   // Handle different kinds of custom GUIs
-		   Class<?> cls = customGUI.getClass();
-		   if( TabPane.class.isAssignableFrom( cls ) ) {
-			   ObservableList<Tab> tabs = ((TabPane) customGUI).getTabs();
-			   for(Tab tab: tabs) {
-				   content = (Parent) tab.getContent();
-				   fillChildren(content);
-			   }
-		   } else if( TitledPane.class.isAssignableFrom( cls ) ) {
-			   content = (Parent) ((TitledPane) customGUI).getContent();
-			   fillChildren(content);
-		   } else {
-			   content = customGUI;
-			   fillChildren(content);
-		   }
+		   		   
+		   // Handle different kinds of custom GUIs recursively
+		   fillChildren(customGUI);
 		   
 		   // now inject the input values for the custom gui based on the slots of the current instance
 		   setObject(this.object);
@@ -73,17 +59,51 @@ public class CustomGUIController {
 	   }
 	   
 	   private void fillChildren(Parent content) {
-		   ObservableList<Node> children = content.getChildrenUnmodifiable();
+		   ObservableList<Node> children;
+		   Parent recall;
+		   
+		   // Handle different kind of contents
+		   Class<?> cls = content.getClass();
+		   if( Pane.class.isAssignableFrom( cls ) ) {
+			   children = ((Pane) content).getChildren();
+			   // content.getChildrenUnmodifiable(); -> bei Parent?
+		   } else if( TabPane.class.isAssignableFrom( cls ) ) {
+			   ObservableList<Tab> tabs = ((TabPane) customGUI).getTabs();
+			   for(Tab tab: tabs) {
+				   recall = (Parent) tab.getContent();
+				   fillChildren(recall);
+			   }
+			   return; // exit condition on recursive call
+		   } else if( TitledPane.class.isAssignableFrom( cls ) ) {
+			   recall = (Parent) ((TitledPane) customGUI).getContent();
+			   fillChildren(recall);
+			   return; // exit condition on recursive call
+		   } else {
+			   System.err.println("Cannot handle the following GUI-Element during loading of CustomGUI: " + cls.getName());
+			   return; // error situation
+		   }
+		   
+		   // Create Map-Entries
 		   String id;
 		   Node obj;
 		   for(Node currEl: children) {
-			   id = currEl.getId();
-			   if( id != null ) {
-				   // this has to be done this way as the Parent class does not allow read access to its children
-				   obj = customGUI.lookup("#" + id);
-				   objToID.put(id, obj);
+			   
+			   // Check if currNode is a Pane on its own (recursive call)
+			   Class<?> currCls = currEl.getClass();
+			   if( Pane.class.isAssignableFrom( currCls ) ) {
+				   fillChildren( (Pane) currEl );
+			   } else {	   
+				   id = currEl.getId();
+				   if( id != null ) {
+					   // this has to be done this way as the Parent class does not allow read access to its children
+					   obj = customGUI.lookup("#" + id);
+					   objToID.put(id, obj);
+				   }
 			   }
 		   }
+		   
+		   // Ähnliche Funktion im Control Center anlegen?
+		   // GUIs je Diagramm anlegen + für ein Level
 	   }
 	   
 	   public void setObject(FmmlxObject object) {
