fdf1de17e6a5737010faa517cc4bb8dfb4b3e51c
Tobias Kleinkauf <tobias.kleinkauf@stud.uni-due.de>
2023-06-10 15:27:18 +0200

    Versuch, IntermediatePoints zu transformieren. Funktioniert aber nicht, deswegen auskommentiert

--- src/tool/clients/fmmlxdiagrams/Edge.java
+++ src/tool/clients/fmmlxdiagrams/Edge.java
@@ -145,12 +145,11 @@ public abstract class Edge<ConcreteNode extends Node> implements CanvasElement {
 		if(!layoutingFinishedSuccesfully) {
 			layoutLabels(view.getDiagram()); 
 			view.getDiagram().redraw();
-		} else {
+		} 
+		else
+		{
+			//transformIntermediates(currentTransform);
 			
-		//HIER!!!	
-			if (currentTransform.)
-		// paintOn bei Klasse
-				
 			Vector<Point2D> points = getAllPoints();
 			/* SHOW ANGLE 
             g.setFill(new Color(.8, .8, .9, 1.));
@@ -287,6 +286,19 @@ public abstract class Edge<ConcreteNode extends Node> implements CanvasElement {
 
 		}
 	}
+	/*
+	private void transformIntermediates(Affine transform)
+	{
+		
+		//Affine transform = g.getTransform();
+	    for (int i = 0; i < intermediatePoints.size(); i++)
+	    {
+	        Point2D point = intermediatePoints.get(i);
+	        Point2D transformedPoint = transform.transform(point);
+	        intermediatePoints.set(i, transformedPoint);
+	    }
+	}
+	*/
 
 	private void drawDecoration(GraphicsContext g, HeadStyle decoration, PortRegion directionForEdge,
 			Point2D pointForEdge) {
