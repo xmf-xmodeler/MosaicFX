b61344456a993118f38ac28cd3a573a3b2693d75
TomSuWi <135829311+TomSuWi@users.noreply.github.com>
2024-09-17 15:58:27 +0200

    Changend LU6
    Mostly completly remade LU6 with an additional class, multiple attribute, and the appropriate conditions to check if the task was solved correctly.
    The condition checks if every class (except horse) has its correct attributes and if car, train, and bus but not horse are children of vehicle.

--- src/tool/xmodeler/didactic_ml/diagram_preperation_actions/GeneralizationSpecializationPreparation.java
+++ src/tool/xmodeler/didactic_ml/diagram_preperation_actions/GeneralizationSpecializationPreparation.java
@@ -16,22 +16,44 @@ public class GeneralizationSpecializationPreparation extends DiagramPreparationA
 }
 	
 	private static void addClasses(FmmlxDiagram diagram) {
-		String vehicleClass = "Vehicle";
+		String busClass = "Bus";
 		String trainClass = "Train";
 		String carClass = "Car";
 		String horseClass = "Horse";
 		
-		createMetaClass(diagram, 1, vehicleClass, new int[]{400, 0});
-		createAttributeOnLevelNull(diagram, vehicleClass, "horsePower", "Integer");
-		createAttributeOnLevelNull(diagram, vehicleClass, "numberOfPassengers", "Integer");
-
+		createMetaClass(diagram, 1, busClass, new int[]{400, 0});
+		createAttributeOnLevelNull(diagram, busClass, "lineNumber", "String");
+		createAttributeOnLevelNull(diagram, busClass, "maxNumberOfPassengers", "Integer");
+		createAttributeOnLevelNull(diagram, busClass, "constructionDate", "Date");
+		addGetAgeFunction(diagram, busClass);
+		
 		createMetaClass(diagram, 1, trainClass, new int[]{0, 0});
-		createAttributeOnLevelNull(diagram, trainClass, "numberOfWheels", "Integer");
+		createAttributeOnLevelNull(diagram, trainClass, "trainCategory", "String");
+		createAttributeOnLevelNull(diagram, trainClass, "usedForPublicTransportation", "Boolean");
+		createAttributeOnLevelNull(diagram, trainClass, "maxNumberOfPassengers", "Integer");
+		createAttributeOnLevelNull(diagram, trainClass, "constructionDate", "Date");
+		addGetAgeFunction(diagram, trainClass);
 		
 		createMetaClass(diagram, 1, carClass, new int[]{200, 0});
-		createAttributeOnLevelNull(diagram, carClass, "numberOfWheels", "Integer");
+		createAttributeOnLevelNull(diagram, carClass, "brandName", "String");
+		createAttributeOnLevelNull(diagram, carClass, "hasBoardComputer", "Boolean");
+		createAttributeOnLevelNull(diagram, carClass, "maxNumberOfPassengers", "Integer");
+		createAttributeOnLevelNull(diagram, carClass, "constructionDate", "Date");
+		addGetAgeFunction(diagram, carClass);
 		
 		createMetaClass(diagram, 1, horseClass, new int[]{600, 0});
-		createAttributeOnLevelNull(diagram, horseClass, "horseName", "String");
+		createAttributeOnLevelNull(diagram, horseClass, "maxNumberOfPassengers", "Integer");
+		createAttributeOnLevelNull(diagram, horseClass, "dateOfBirth", "Date");
+		createAttributeOnLevelNull(diagram, horseClass, "typeOfBreed", "String");
+		addGetAgeFunction(diagram, horseClass);
+	}
+	
+	private static void addGetAgeFunction(FmmlxDiagram diagram, String className) {
+		String dateName="constructionDate";
+		if(className=="Horse") {
+			dateName="dateOfBirth";
+		}
+		String funBody = "@Operation getAge[monitor=true]():XCore::Integer if self."+dateName+" <> null then self.dateOfBirth.age() else \"No date of birth entered\" end end";
+		diagram.getComm().addOperation(diagram.getID(), diagram.getClassPath(className), 0, funBody);
 	}
 }
--- src/tool/xmodeler/didactic_ml/sucess_conditions/GeneralizationSpecializationConditions.java
+++ src/tool/xmodeler/didactic_ml/sucess_conditions/GeneralizationSpecializationConditions.java
@@ -1,34 +1,100 @@
 package tool.xmodeler.didactic_ml.sucess_conditions;
 
 import tool.clients.fmmlxdiagrams.FmmlxObject;
+import tool.xmodeler.didactic_ml.backend_aux.DiagramConditionChecks;
 import tool.xmodeler.didactic_ml.self_assessment_test_tasks.SelfAssessmentTestTasks;
 
 public class GeneralizationSpecializationConditions extends SuccessCondition {
-
+	
+	FmmlxObject car;
+	FmmlxObject train;
+	FmmlxObject bus;
+	FmmlxObject horse;
+	FmmlxObject vehicle;
+	
 	@Override
 	public boolean checkSuccessCondition() {
 		switch (SelfAssessmentTestTasks.getPrecedence(diagram.getViewPane().getCurrentTaskName())) {
 		case 1:
 			return true;	//Just press the button
 		case 2:
-			return checkParent();
+			return checkPhase2();
 		case 3:
 			return true;		//Just the end button
 		}
 		return true;
 	}
 	
+	private boolean checkPhase2() {
+		car = diagram.getObjectByName("Car");
+		train = diagram.getObjectByName("Train");
+		bus = diagram.getObjectByName("Bus");
+		horse = diagram.getObjectByName("Horse");
+		
+		if(diagram.getObjectByName("Vehicle")!=null) {
+		vehicle = diagram.getObjectByName("Vehicle");}
+		else {return false;}
+		
+		return checkParent() && checkAllAttributes();
+	}
+	
 	private boolean checkParent() {	//not checking for horse because that one is deemed correct either way
-		boolean correctParent = false;
-		FmmlxObject car = diagram.getObjectByName("Car");
-		FmmlxObject train = diagram.getObjectByName("Train");
-		FmmlxObject vehicle = diagram.getObjectByName("Vehicle");
+		boolean correctParent = false;	
 		if(car.getAllAncestors().size()>0 && train.getAllAncestors().size()>0) {	
-			if(car.getAllAncestors().get(0)==vehicle && train.getAllAncestors().get(0)==vehicle){
+			if(car.getAllAncestors().get(0)==vehicle && train.getAllAncestors().get(0)==vehicle && bus.getAllAncestors().get(0)==vehicle && horse.getAllAncestors().size()==0){
 				correctParent = true;
 			}
 		}
 		return correctParent;
+	}
+	
+	private boolean checkAllAttributes() {
+		if(!checkAttributes(car)) {
+			return false;
+		}
+		if(!checkAttributes(train)) {
+			return false;
+		}
+		if(!checkAttributes(bus)) {
+			return false;
+		}
+		if(!checkAttributes(vehicle)) {
+			return false;
+		}
+		return true;
+	}
+	
+	private boolean checkAttributes(FmmlxObject o) {
+		if(DiagramConditionChecks.hasAttributeOfType(o, "constructionDate", "Date") && o!=vehicle) {
+			return false;
+		}
+		if(DiagramConditionChecks.hasAttributeOfType(o, "maxNumberOfPassengers", "Integer") && o!=vehicle && o!=horse) {
+			return false;
+		}
+		if(o==car) {
+			if(!(DiagramConditionChecks.hasAttributeOfType(o, "hasBoardComputer", "Boolean") && DiagramConditionChecks.hasAttributeOfType(o, "brandName", "String"))) {
+			return false;
+			}
+		}
+		if(o==train) {
+			if(!(DiagramConditionChecks.hasAttributeOfType(o, "usedForPublicTransportation", "Boolean") && DiagramConditionChecks.hasAttributeOfType(o, "trainCategory", "String"))) {
+			return false;
+			}
+		}
+		if(o==bus) {
+			if(!( DiagramConditionChecks.hasAttributeOfType(o, "lineNumber", "String"))) {
+			return false;
+			}
+		}
+		if(o==vehicle) {
+			if(!(DiagramConditionChecks.hasAttributeOfType(o, "constructionDate", "Date") &&
+					DiagramConditionChecks.hasAttributeOfType(o, "maxNumberOfPassengers", "Integer") &&
+					DiagramConditionChecks.hasAttributeOfType(o, "numberOfSeats", "Integer") &&
+					DiagramConditionChecks.hasAttributeOfType(o, "isElectric", "Boolean"))) {
+			return false;
+			}
+		}
+		return true;
 	}
 
 }
