a20d4ca33a9127367d445336228a8cec21a21003
TomSuWi <135829311+TomSuWi@users.noreply.github.com>
2024-09-26 15:01:57 +0200

    Added Tutorial Link and fixed an missing integer

--- src/tool/xmodeler/ControlCenter.java
+++ src/tool/xmodeler/ControlCenter.java
@@ -165,6 +165,9 @@ public class ControlCenter extends Stage {
 			MenuItem getUMLInformationItem = new MenuItem("Get Information on UML-MX");
 			getUMLInformationItem.setOnAction(e->openWebpage("https://www.wi-inf.uni-due.de/LE4MM/uml-mx/"));
 			
+			MenuItem getOnlineTutorial = new MenuItem("Open Online Tutorial");
+			getOnlineTutorial.setOnAction(e->openWebpage("https://www.wi-inf.uni-due.de/LE4MM/uml-mx-tutorials/"));
+			
 			MenuItem getSourceCodeItem = new MenuItem("Get Source Code");
 			getSourceCodeItem.setOnAction(e->openWebpage("https://github.com/xmf-xmodeler"));
 			
