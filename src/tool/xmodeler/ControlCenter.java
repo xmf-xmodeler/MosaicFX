366f655960061acd447424273b52fbb429fae7e7
admin <admin@132.252.52.142>
2025-05-13 12:46:30 +0200

    changed title of ControlCenter

--- src/tool/xmodeler/ControlCenter.java
+++ src/tool/xmodeler/ControlCenter.java
@@ -85,7 +85,7 @@ public class ControlCenter extends Stage {
 	}
 
 	public ControlCenter() {
-		setTitle("XModelerML\u00a9 Control Center");
+		setTitle("My Test title");//("XModelerML\u00a9 Control Center");
 		setResizable(false);
 		if(PropertyManager.isInDidacticMode()) {
 			setTitle("UML-MX\u00a9 Control Center");
